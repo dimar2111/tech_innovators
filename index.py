b2d9b8d781e79c2a0c468c9bc36fa11f55dd3ee9
unknown <dimarcoariteucb@gmail.com>
2024-09-30 16:34:22 -0400

    Primer commit

--- index.py
+++ index.py
@@ -1,12 +1,24 @@
 from flask import Flask, render_template
 
+# Inicializa la aplicación Flask
 app = Flask(__name__)
 
 @app.route('/')
 def home():
+    """Renderiza la página de inicio."""
     return render_template('home.html')
+
+@app.route('/contenido')
+def contenido():
+    """Renderiza la página de contenido."""
+    return render_template('contenido.html')
+
 @app.route('/about')
 def about():
+    """Renderiza la página de información."""
     return render_template('about.html')
+
 if __name__ == '__main__':
+    # Ejecuta la aplicación en modo de depuración
     app.run(debug=True)
+
